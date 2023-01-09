39e270392ccdc909b38f83891c92eaad3587ccac
Krishna Prasad <peramkrishnaprasad@gmail.com>
2023-01-10 02:17:44 +0530

    first broooooooo

---
+++ MySelenium/src/com/theSelenium/Basic.java
@@ -0,0 +1,11 @@
+package com.theSelenium;
+
+public class Basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("H hhhjlo");
+      
+	}
+
+}
